c58b2bb6a3e2686f34d491dd8d4040e986f4385f
Nabeel111 <nabeel2014@namal.edu.pk>
2016-11-08 17:08:35 +0500

    update file

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int var;
+   cout<<"enter a number";
+   cin>>var;
+    if (var%2==0)
+
+{
+
+
+
+    cout<<"even number";
+}
+    else
+{
+
+
+    cout<<"odd number";
+
+}
+
+    return 0;
+}
